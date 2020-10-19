95c4faac313c39b92e189edcb7f5b0659ddfd1d2
Jorge Amorim <jfbam@iscte-iul.pt>
2020-10-20 00:49:03 +0100

    4. Avalia médicos - funcional e testado

--- avalia_medicos.sh
+++ avalia_medicos.sh
@@ -4,7 +4,8 @@ do
    linha=$p
    ncons=$(echo $linha | cut -d ";" -f5)
    aval=$(echo $linha | cut -d ";" -f6)
-   if $ncons -gt 6 && $aval -lt 5; then
+   if [ $ncons -gt 6 ] && [ $aval -lt 5 ]; then
+      echo "Entrou aqui"
       echo $linha >> lista_negra_medicos.txt
    fi
 done < medicos.txt 
