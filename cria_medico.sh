a7649809ada9ae32a0924471aa8cb8c9e12df99f
Jorge Amorim <jfbam@iscte-iul.pt>
2020-10-20 00:36:24 +0100

    Shell Cria medico a funcionar para todos os req

--- cria_medico.sh
+++ cria_medico.sh
@@ -5,15 +5,14 @@ then
   echo "Não inseriu dados"
   exit
 else
-     if [ -e "$FILE" ]; then
-        touch medicos.txt
+     if [ ! -e "$FILE" ]; then
+	touch medicos.txt
+     fi
+     if grep -q $3 medicos.txt; then
+        echo "Já existe um médico com o numero de cedula $3"
      else
-        if grep -q $3 medicos.txt; then
-           echo "Já existe um médico com o numero de cedula $3"
-        else
-           nomecompleto="$1 $2"
-           echo "$nomecompleto;$3;$4;$s5;0;0;0" >> medicos.txt
-           cat medicos.txt
-        fi
-    fi
+        nomecompleto="$1 $2"
+        echo "$nomecompleto;$3;$4;$s5;0;0;0" >> medicos.txt
+        cat medicos.txt
+     fi
 fi
