7afb534721832aedec30cb121845a5bbe98bdb30
Jorge Amorim <jfbam@iscte-iul.pt>
2020-10-19 23:33:15 +0100

    Alterações no ficheiro Stats.sh

--- stats.sh
+++ stats.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cat medicos.txt | 
+sed -n "/$1/P" pacientes.txt > tempfile.txt
+nmed=$( wc -l tempfile.txt )
+echo "O numero de medicos inscritos numa plataforma da localidade de $1 são $nmed"
+rm -f tempfile.txt 
