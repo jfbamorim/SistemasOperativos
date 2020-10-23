614a5d2aa6984af9de8a8af3209ebbea9bf7b569
Jorge Amorim <jfbam@iscte-iul.pt>
2020-10-23 01:25:48 +0100

    Versão Final Avalia Medicos

--- avalia_medicos.sh
+++ avalia_medicos.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-while IFS="" read -r p || [ -n "$p" ]
-do
-   linha=$p
-   ncons=$(echo $linha | cut -d ";" -f5)
-   aval=$(echo $linha | cut -d ";" -f6)
-   if [ $ncons -gt 6 ] && [ $aval -lt 5 ]; then
-      echo $linha >> lista_negra_medicos.txt
-   fi
-done < medicos.txt 
-cat lista_negra_medicos.txt
+FILE="medicos.txt"
+
+if [ -f "$FILE" ]; then
+   while IFS="" read -r p || [ -n "$p" ]
+   do
+      linha=$p
+      ncons=$(echo $linha | cut -d ";" -f5)
+      aval=$(echo $linha | cut -d ";" -f6)
+      if [ $ncons -gt 6 ] && [ $aval -lt 5 ]; then
+         echo $linha >> lista_negra_medicos.txt
+      fi
+   done < medicos.txt 
+   cat lista_negra_medicos.txt
+else
+   echo "Ficheiro dos médicos não existe"
+fi
